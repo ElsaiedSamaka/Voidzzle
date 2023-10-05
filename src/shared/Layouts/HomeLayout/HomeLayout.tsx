c5f1f4baecde2bae2c31525cbe157b30a329bf86
Saied <elsaied.samaka1997@gmail.com>
2023-10-05 16:18:18 +0300

    commit

--- src/shared/Layouts/HomeLayout/HomeLayout.tsx
+++ src/shared/Layouts/HomeLayout/HomeLayout.tsx
@@ -3,10 +3,10 @@ const HomeLayout = ({ children }) => {
     <section className="home-layout p-2 bg-purple-500 w-full">
       <nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900 w-full fixed top-14 right-0 md:left-64 z-10">
         {/* Home Navbar */}
-        <div className="flex flex-wrap justify-between items-center mx-auto w-full p-4">
+        <div className="flex flex-wrap justify-between items-center  p-4">
           <a href="https://flowbite.com" className="flex items-center ">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Hello Saied
             </span>
           </a>
           <button
@@ -36,7 +36,7 @@ const HomeLayout = ({ children }) => {
           {/* submenu */}
           <div
             id="mega-menu-full"
-            className="items-center  justify-between font-medium  w-full md:flex md:w-auto md:order-1"
+            className="items-center justify-between font-medium  w-full md:flex "
           >
             <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
@@ -44,7 +44,7 @@ const HomeLayout = ({ children }) => {
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                   aria-current="page"
                 >
-                  Home
+                  All
                 </a>
               </li>
               <li>
@@ -53,7 +53,7 @@ const HomeLayout = ({ children }) => {
                   data-collapse-toggle="mega-menu-full-dropdown"
                   className="flex items-center justify-between w-full py-2 pl-3 pr-4  text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Company{" "}
+                  Vehicles
                   <svg
                     className="w-2.5 h-2.5 ml-2.5"
                     aria-hidden="true"
@@ -73,17 +73,27 @@ const HomeLayout = ({ children }) => {
               </li>
               <li>
                 <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                  Marketplace
+                  Properties
                 </a>
               </li>
               <li>
                 <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                  Resources
+                  Mobiles & Tablets
                 </a>
               </li>
               <li>
                 <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                  Contact
+                  Jobs
+                </a>
+              </li>
+              <li>
+                <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                  Electronics & Appliances
+                </a>
+              </li>
+              <li>
+                <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                  Furniture & Decor
                 </a>
               </li>
             </ul>
