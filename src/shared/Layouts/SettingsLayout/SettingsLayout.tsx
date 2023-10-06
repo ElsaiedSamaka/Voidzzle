589177b287010ec9b38b685abca31f5afbfee702
Saied <elsaied.samaka1997@gmail.com>
2023-10-06 19:19:34 +0300

    initialization

--- src/shared/Layouts/SettingsLayout/SettingsLayout.tsx
+++ src/shared/Layouts/SettingsLayout/SettingsLayout.tsx
@@ -1,9 +1,9 @@
 const SettingsLayout = ({ children }) => {
   return (
-    <section className="settings-layout md:ml-64">
+    <section className="settings-layout ">
       <div className=" max-w-screen-2xl">
         <div className="grid pt-3 pb-10 grid-cols-10">
-          <div className="col-span-10 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
+          <div className="col-span-10 overflow-hidden rounded-2xl bg-gray-100 px-8 mx-8 sm:shadow">
             {children}
           </div>
         </div>
