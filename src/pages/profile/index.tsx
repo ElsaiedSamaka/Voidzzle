04a4b950eb522fd61c0d527c72324de5547bc3a9
Saied <elsaied.samaka1997@gmail.com>
2023-10-06 21:35:08 +0300

    initialization

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,9 +3,9 @@ import { ProfileLayout, RootLayout } from "shared";
 const Profile = () => {
   return (
     <>
-      Profile (My adds,My orders ,My Saved ,My wishlist ,My payments , Billing)
+      Profile (My adds , Billing)
       <div className="mx-4  sm:mx-8 xl:mx-auto">
-        <h1 className="border-b py-6 text-4xl font-semibold">Settings</h1>
+        <h1 className="border-b py-6 text-4xl font-semibold">Profile</h1>
         <div className="grid grid-cols-8 pt-3 pb-10 sm:grid-cols-10">
           <div className="col-span-10 rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
             <div className="pt-4">
@@ -104,6 +104,265 @@ const Profile = () => {
                 </label>
               </div>
             </div>
+            <hr />
+
+            <div className="relative overflow-x-auto shadow-sm sm:rounded-lg my-2">
+              <p className="mb-6 bg-gray-100 py-1 text-center text-lg font-medium">
+                My wishlist
+              </p>
+              <table className="w-full text-sm text-left text-gray-500 :text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 :bg-gray-700 :text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      <span className="sr-only">Image</span>
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Product
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Qty
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Price
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="bg-white border-b :bg-gray-800 :border-gray-700 hover:bg-gray-50 :hover:bg-gray-600">
+                    <td className="w-32 p-4">
+                      <img
+                        src="/docs/images/products/apple-watch.png"
+                        alt="Apple Watch"
+                      />
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-gray-900 :text-white">
+                      Apple Watch
+                    </td>
+                    <td className="px-6 py-4">
+                      <div className="flex items-center space-x-3">
+                        <button
+                          className="inline-flex items-center justify-center p-1 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 :bg-gray-800 :text-gray-400 :border-gray-600 :hover:bg-gray-700 :hover:border-gray-600 :focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 2"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M1 1h16"
+                            />
+                          </svg>
+                        </button>
+                        <div>
+                          <input
+                            type="number"
+                            id="first_product"
+                            className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 :bg-gray-700 :border-gray-600 :placeholder-gray-400 :text-white :focus:ring-blue-500 :focus:border-blue-500"
+                            required
+                          />
+                        </div>
+                        <button
+                          className="inline-flex items-center justify-center h-6 w-6 p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 :bg-gray-800 :text-gray-400 :border-gray-600 :hover:bg-gray-700 :hover:border-gray-600 :focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 18"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M9 1v16M1 9h16"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-gray-900 :text-white">
+                      $599
+                    </td>
+                    <td className="px-6 py-4">
+                      <a
+                        href="#"
+                        className="font-medium text-red-600 :text-red-500 hover:underline"
+                      >
+                        Remove
+                      </a>
+                    </td>
+                  </tr>
+                  <tr className="bg-white border-b :bg-gray-800 :border-gray-700 hover:bg-gray-50 :hover:bg-gray-600">
+                    <td className="w-32 p-4">
+                      <img
+                        src="/docs/images/products/imac.png"
+                        alt="Apple Imac"
+                      />
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-gray-900 :text-white">
+                      Imac 27"
+                    </td>
+                    <td className="px-6 py-4">
+                      <div className="flex items-center space-x-3">
+                        <button
+                          className="inline-flex items-center justify-center p-1 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 :bg-gray-800 :text-gray-400 :border-gray-600 :hover:bg-gray-700 :hover:border-gray-600 :focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 2"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M1 1h16"
+                            />
+                          </svg>
+                        </button>
+                        <div>
+                          <input
+                            type="number"
+                            id="first_product"
+                            className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 :bg-gray-700 :border-gray-600 :placeholder-gray-400 :text-white :focus:ring-blue-500 :focus:border-blue-500"
+                            required
+                          />
+                        </div>
+                        <button
+                          className="inline-flex items-center justify-center h-6 w-6 p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 :bg-gray-800 :text-gray-400 :border-gray-600 :hover:bg-gray-700 :hover:border-gray-600 :focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 18"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M9 1v16M1 9h16"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-gray-900 :text-white">
+                      $2499
+                    </td>
+                    <td className="px-6 py-4">
+                      <a
+                        href="#"
+                        className="font-medium text-red-600 :text-red-500 hover:underline"
+                      >
+                        Remove
+                      </a>
+                    </td>
+                  </tr>
+                  <tr className="bg-white border-b :bg-gray-800 :border-gray-700 hover:bg-gray-50 :hover:bg-gray-600">
+                    <td className="w-32 p-4">
+                      <img
+                        src="/docs/images/products/iphone-12.png"
+                        alt="Iphone 12"
+                      />
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-gray-900 :text-white">
+                      Iphone 12
+                    </td>
+                    <td className="px-6 py-4">
+                      <div className="flex items-center space-x-3">
+                        <button
+                          className="inline-flex items-center justify-center p-1 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 :bg-gray-800 :text-gray-400 :border-gray-600 :hover:bg-gray-700 :hover:border-gray-600 :focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 2"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M1 1h16"
+                            />
+                          </svg>
+                        </button>
+                        <div>
+                          <input
+                            type="number"
+                            id="first_product"
+                            className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 :bg-gray-700 :border-gray-600 :placeholder-gray-400 :text-white :focus:ring-blue-500 :focus:border-blue-500"
+                            required
+                          />
+                        </div>
+                        <button
+                          className="inline-flex items-center justify-center h-6 w-6 p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 :bg-gray-800 :text-gray-400 :border-gray-600 :hover:bg-gray-700 :hover:border-gray-600 :focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 18"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M9 1v16M1 9h16"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-gray-900 :text-white">
+                      $999
+                    </td>
+                    <td className="px-6 py-4">
+                      <a
+                        href="#"
+                        className="font-medium text-red-600 :text-red-500 hover:underline"
+                      >
+                        Remove
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
             <div className="amx-auto mb-10 overflow-hidden rounded-lg border bg-white">
               <p className="mb-6 bg-gray-100 py-1 text-center text-lg font-medium">
                 Billing History
