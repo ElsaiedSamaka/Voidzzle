bbd37b0cbaccf4fc4bc84e775ef4c440977259ff
Saied <elsaied.samaka1997@gmail.com>
2023-10-06 19:22:32 +0300

    initialization

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -14,6 +14,7 @@ import ProfileLayout from "./Layouts/ProfileLayout/ProfileLayout";
 import MessagesLayout from "./Layouts/MessagesLayout/MessagesLayout";
 import NotificationsLayout from "./Layouts/NotificationsLayout/NotificationsLayout";
 import Anchor from "./Common/Anchor/Anchor";
+import Breadcrumb from "./Common/Breadcrumb/Breadcrumb";
 export {
   Aside,
   Card,
@@ -31,5 +32,6 @@ export {
   NotificationsLayout,
   MessagesLayout,
   Anchor,
+  Breadcrumb,
 };
 
