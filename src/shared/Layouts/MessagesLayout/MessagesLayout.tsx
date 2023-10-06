f2007a30c7a8afb28198027a6f57648a510ac2f1
Saied <elsaied.samaka1997@gmail.com>
2023-10-06 19:29:35 +0300

    initialization

--- src/shared/Layouts/MessagesLayout/MessagesLayout.tsx
+++ src/shared/Layouts/MessagesLayout/MessagesLayout.tsx
@@ -2,14 +2,10 @@ import React from "react";
 
 const MessagesLayout = ({ children }) => {
   return (
-    <section className="settings-layout md:ml-64">
-      <div className=" max-w-screen-xl">
-        <h1 className="border-b py-6 text-4xl font-semibold">Settings</h1>
-        <div className="grid grid-cols-8 pt-3 pb-10 sm:grid-cols-10">
-          <div className="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
-            <div className="pt-4">
-              <h1 className="py-2 text-2xl font-semibold">Settings</h1>
-            </div>
+    <section className="messages-layout ">
+      <div className=" max-w-screen-2xl">
+        <div className="grid pt-3 pb-10 grid-cols-10">
+          <div className="col-span-10 overflow-hidden rounded-2xl bg-gray-100 px-8 mx-8 sm:shadow">
             {children}
           </div>
         </div>
