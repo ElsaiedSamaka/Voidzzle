267170f95c9799904a362f41bd4750958cb1ce05
Saied <elsaied.samaka1997@gmail.com>
2023-10-07 09:58:55 +0300

    initialization

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { RootLayout } from "shared";
+
+const Products = () => {
+  return <div>Products</div>;
+};
+Products.getLayout = (page) => {
+  return <RootLayout>{page}</RootLayout>;
+};
+export default Products;
