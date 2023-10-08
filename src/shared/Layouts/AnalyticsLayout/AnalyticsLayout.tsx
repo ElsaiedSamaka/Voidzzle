004974a98e55d376db87d8374a1a00fd9c67a6c7
Saied <elsaied.samaka1997@gmail.com>
2023-10-08 15:04:39 +0300

    home page setup

--- src/shared/Layouts/AnalyticsLayout/AnalyticsLayout.tsx
+++ src/shared/Layouts/AnalyticsLayout/AnalyticsLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AnalyticsLayout = ({children}) => {
   return (
-    <section className="profile-layout ">
+    <section className="analytics-layout mt-10">
       <div className=" max-w-screen-2xl">
         <div className="grid pt-3 pb-10 grid-cols-10">
           <div className="col-span-10 overflow-hidden rounded-2xl bg-gray-100 px-8 mx-8 sm:shadow">
