c5e5991d1fe72e212d2bb57022ce2c2486104539
Saied <elsaied.samaka1997@gmail.com>
2023-10-08 19:20:45 +0300

    home page setup

--- src/shared/Common/Tabs/Tabs.tsx
+++ src/shared/Common/Tabs/Tabs.tsx
@@ -34,15 +34,3 @@ const Tabs = ({ items }) => {
 };
 
 export default Tabs;
-
-// not active
-//  <a className="whitespace-nowrap inline-flex rounded-md py-2 px-4 items-center  text-sm  text-gray-400 bg-gray-100  transition-all duration-200 hover:cursor-pointer ease-in-out  hover:text-gray-700 hover:shadow">
-//    {" "}
-//    Products{" "}
-//  </a>;
-
-// active
-//  <a className="whitespace-nowrap inline-flex rounded-md items-center py-2 px-4 text-sm  text-black bg-white shadow font-semibold transition-all duration-200 hover:cursor-pointer ease-in-out">
-//    {" "}
-//    Tasks{" "}
-//  </a>;
