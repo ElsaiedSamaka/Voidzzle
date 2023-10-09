349adbfddab744875ea2293d05e3c1c16c099d50
Saied <elsaied.samaka1997@gmail.com>
2023-10-09 09:11:49 +0300

    home page setup

--- src/views/Innova.views/content.innova/Content.tsx
+++ src/views/Innova.views/content.innova/Content.tsx
@@ -1,122 +1,22 @@
 import { TabContext, useTabContext } from "core/context/TabContext";
 import React, { useContext } from "react";
 import { Table } from "shared";
+import Ads from "../ads.innova/Ads";
+import Sales from "../sales.innova/Sales";
+import Products from "../products.innova/Products";
+import Users from "../users.innova/Users";
+import Tasks from "../tasks.innova/Tasks";
 
 const Content = () => {
   const { state } = useTabContext();
   const { selectedTabIndex } = state;
   return (
     <div>
-      {selectedTabIndex === 0 && (
-        <Table
-          _config={{
-            title: "Products",
-            _head: {
-              show: true,
-              _actions: {
-                show: true,
-                addtions: true,
-                actions: true,
-                filteration: true,
-              },
-              _search: {
-                show: true,
-              },
-            },
-            _body: { isTrue: true, perPage: 7 },
-            _footer: { isTrue: true },
-          }}
-          data={[]}
-        />
-      )}
-      {selectedTabIndex === 1 && (
-        <Table
-          _config={{
-            title: "Users",
-            _head: {
-              show: true,
-              _actions: {
-                show: true,
-                addtions: true,
-                actions: true,
-                filteration: true,
-              },
-              _search: {
-                show: true,
-              },
-            },
-            _body: { isTrue: true, perPage: 7 },
-            _footer: { isTrue: true },
-          }}
-          data={[]}
-        />
-      )}
-      {selectedTabIndex === 2 && (
-        <Table
-          _config={{
-            title: "Products",
-            _head: {
-              show: true,
-              _actions: {
-                show: true,
-                addtions: true,
-                actions: true,
-                filteration: true,
-              },
-              _search: {
-                show: true,
-              },
-            },
-            _body: { isTrue: true, perPage: 7 },
-            _footer: { isTrue: true },
-          }}
-          data={[]}
-        />
-      )}
-      {selectedTabIndex === 3 && (
-        <Table
-          _config={{
-            title: "Sales",
-            _head: {
-              show: true,
-              _actions: {
-                show: true,
-                addtions: true,
-                actions: true,
-                filteration: true,
-              },
-              _search: {
-                show: true,
-              },
-            },
-            _body: { isTrue: true, perPage: 7 },
-            _footer: { isTrue: true },
-          }}
-          data={[]}
-        />
-      )}
-      {selectedTabIndex === 4 && (
-        <Table
-          _config={{
-            title: "Ads",
-            _head: {
-              show: true,
-              _actions: {
-                show: true,
-                addtions: true,
-                actions: true,
-                filteration: true,
-              },
-              _search: {
-                show: true,
-              },
-            },
-            _body: { isTrue: true, perPage: 7 },
-            _footer: { isTrue: true },
-          }}
-          data={[]}
-        />
-      )}
+      {selectedTabIndex === 0 && <Products />}
+      {selectedTabIndex === 1 && <Users />}
+      {selectedTabIndex === 2 && <Tasks />}
+      {selectedTabIndex === 3 && <Sales />}
+      {selectedTabIndex === 4 && <Ads />}
     </div>
   );
 };
