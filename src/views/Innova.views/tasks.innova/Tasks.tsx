b88bef1015ea7e9872bb2d59056a55eb05684e5e
Saied <elsaied.samaka1997@gmail.com>
2023-10-09 09:12:55 +0300

    home page setup

--- src/views/Innova.views/tasks.innova/Tasks.tsx
+++ src/views/Innova.views/tasks.innova/Tasks.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Table } from "shared";
 
 const Tasks = () => {
   return (
-    <div>Tasks</div>
-  )
-}
+    <Table
+      _config={{
+        title: "Tasks",
+        _head: {
+          show: true,
+          _actions: {
+            show: true,
+            addtions: true,
+            actions: true,
+            filteration: true,
+          },
+          _search: {
+            show: true,
+          },
+        },
+        _body: { isTrue: true, perPage: 7 },
+        _footer: { isTrue: true },
+      }}
+      data={[]}
+    />
+  );
+};
 
 export default Tasks
