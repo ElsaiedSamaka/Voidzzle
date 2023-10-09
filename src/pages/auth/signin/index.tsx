99044d5b15c617b9f6f45b0ea8cc5c5e93137a7c
Saied <elsaied.samaka1997@gmail.com>
2023-10-09 08:50:56 +0300

    home page setup

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -6,7 +6,7 @@ const Signin = () => {
   return (
     <>
       <div className="p-10 w-screen h-screen absolute z-10">
-        <div className="flex h-[99%] rounded-xl border flex-col md:flex-row md:pr-10 bg-white/90  ">
+        <div className="flex h-[99%] rounded-xl border flex-col md:flex-row md:pr-10 backdrop-blur  ">
           <div className="max-w-md rounded-3xl bg-gradient-to-t from-red-500 via-red-700 to-red-600 px-4 py-10 text-white sm:px-10 md:m-6 md:mr-8">
             <p className="mb-20 font-bold tracking-wider">Voidzzle</p>
             <p className="mb-4 text-3xl font-bold md:text-4xl md:leading-snug">
@@ -27,10 +27,10 @@ const Signin = () => {
                   <img
                     className="h-10 w-10 rounded-full object-cover"
                     src="/images/y9s3xOJV6rnQPKIrdPYJy.png"
-                    alt="Simon Lewis"
+                    alt=""
                   />
                   <p className="ml-4 w-56">
-                    <strong className="block font-medium">Simon Lewis</strong>
+                    <strong className="block font-medium">Lorem ipsum</strong>
                     <span className="text-xs text-gray-200">
                       {" "}
                       Published 12 Bestsellers{" "}
@@ -40,50 +40,13 @@ const Signin = () => {
               </div>
             </div>
           </div>
-          <div className="px-4 py-20 z-10 ">
+          <div className="px-4 py-20 z-10 w-full max-w-lg">
             <h2 className="mb-2 text-3xl font-bold">Sign In</h2>
             <div className="mb-10 block font-bold text-gray-600">
               don't have an account ?{" "}
               <Link href="/auth/signup">
                 <span className="hover:underline">Sign Up</span>
               </Link>
-            </div>
-            <p className="mb-1 font-medium text-gray-500">Looking for?</p>
-            <div className="mb-6 flex flex-col gap-y-2 gap-x-4 lg:flex-row">
-              <div className="relative flex w-56 items-center justify-center rounded-xl bg-gray-50 px-4 py-3 font-medium text-gray-700">
-                <input
-                  className="peer hidden"
-                  type="radio"
-                  name="radio"
-                  id="radio1"
-                  defaultChecked
-                />
-                <label
-                  className="peer-checked:border-red-600 peer-checked:bg-red-200 absolute top-0 h-full w-full cursor-pointer rounded-xl border"
-                  htmlFor="radio1"
-                >
-                  {" "}
-                </label>
-                <div className="peer-checked:border-transparent peer-checked:bg-red-600 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-red-600 ring-offset-2" />
-                <span className="pointer-events-none z-10">Projects</span>
-              </div>
-              <div className="relative flex w-56 items-center justify-center rounded-xl bg-gray-50 px-4 py-3 font-medium text-gray-700">
-                <input
-                  className="peer hidden"
-                  type="radio"
-                  name="radio"
-                  id="radio3"
-                  defaultChecked
-                />
-                <label
-                  className="peer-checked:border-red-600 peer-checked:bg-red-200 absolute top-0 h-full w-full cursor-pointer rounded-xl border"
-                  htmlFor="radio3"
-                >
-                  {" "}
-                </label>
-                <div className="peer-checked:border-transparent peer-checked:bg-red-600 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-red-600 ring-offset-2" />
-                <span className="pointer-events-none z-10">Job</span>
-              </div>
             </div>
             <p className="mb-1 font-medium text-gray-500">Email</p>
             <div className="mb-4 flex flex-col">
