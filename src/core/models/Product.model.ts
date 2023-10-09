db9d0347d22d3a9c2e798feea94afed714c9f94c
Saied <elsaied.samaka1997@gmail.com>
2023-10-09 09:25:17 +0300

    home page setup

---
+++ src/core/models/Product.model.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  id?: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  availablity: boolean;
+  product_img: string;
+  cloudinary_id?: string;
+  categoryId?: number;
+};
--- src/core/models/index.ts
+++ src/core/models/index.ts
@@ -2,5 +2,6 @@ import { Conversation } from "./Conversation.model";
 import { Message } from "./Message.model";
 import { Todo } from "./Todo.model";
 import { User } from "./User.model";
-export type { Conversation, Message, Todo, User };
+import { Product } from "./Product.model";
+export type { Conversation, Message, Todo, User, Product };
 
