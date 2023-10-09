112997daeae8b6ec0f94c75a96c15ef364f251fb
Saied <elsaied.samaka1997@gmail.com>
2023-10-09 10:16:33 +0300

    home page setup

--- src/views/Innova.views/products.innova/Products.tsx
+++ src/views/Innova.views/products.innova/Products.tsx
@@ -1,4 +1,5 @@
 import { RootState } from "core/store";
+import { getProductsThunk } from "core/store/products/products.thunk";
 import { getTodosThunk } from "core/store/todos/todos.thunk";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,11 +7,11 @@ import { Table } from "shared";
 
 const Products = () => {
   const dispatch = useDispatch();
-  const todosSlice = useSelector((state: RootState) => state.todos);
-  console.log("todoSlice", todosSlice);
+  const productsSlice = useSelector((state: RootState) => state.products);
+  console.log("productsSlice", productsSlice);
   // get todos from api just when component is mounted
   useEffect(() => {
-    dispatch(getTodosThunk());
+    dispatch(getProductsThunk());
   }, [dispatch]);
   return (
     <Table
