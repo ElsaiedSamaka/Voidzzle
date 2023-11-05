c5752377cd125c7912b44bbe1d0c3b1acc267602
Saied <elsaied.samaka1997@gmail.com>
2023-11-05 15:30:48 +0200

    code enhance

--- src/shared/Table/TableHead/TableHead.tsx
+++ src/shared/Table/TableHead/TableHead.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Form from "shared/Forms/Form";
 import Dailog from "shared/Modals/Dailog/Dailog";
-
+import classNames from "classnames";
 const TableHead = ({
   _config,
   data,
@@ -10,8 +10,12 @@ const TableHead = ({
   handleAddetionDispatch,
 }) => {
   const [showAddModal, toggleAddetionModal] = useState(false);
+  const [showActionsPopover, setShowActionsPopover] = useState(false);
   function handleModalToggle() {
     toggleAddetionModal(!showAddModal);
+  }
+  function handleActionToggle() {
+    setShowActionsPopover(!showActionsPopover);
   }
   return (
     <>
@@ -73,48 +77,44 @@ const TableHead = ({
               </button>
             )}
 
-            <div className="flex items-center space-x-3 w-full md:w-auto">
+            <div className="flex items-center space-x-3 w-full md:w-auto ">
               {_config.actions && (
                 <>
-                  <button
-                    id="actionsDropdownButton"
-                    data-dropdown-toggle="actionsDropdown"
-                    className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 :focus:ring-gray-700 :bg-gray-800 :text-gray-400 :border-gray-600 :hover:text-white :hover:bg-gray-700"
-                    type="button"
-                  >
-                    <svg
-                      className="-ml-1 mr-1.5 w-5 h-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
+                  <div className="relative">
+                    <button
+                      onClick={handleActionToggle}
+                      id="actionsDropdownButton"
+                      data-dropdown-toggle="actionsDropdown"
+                      className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 :focus:ring-gray-700 :bg-gray-800 :text-gray-400 :border-gray-600 :hover:text-white :hover:bg-gray-700"
+                      type="button"
                     >
-                      <path
-                        clipRule="evenodd"
-                        fillRule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      />
-                    </svg>
-                    Actions
-                  </button>
-                  <div
-                    id="actionsDropdown"
-                    className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow :bg-gray-700 :divide-gray-600"
-                  >
-                    <ul
-                      className="py-1 text-sm text-gray-700 :text-gray-200"
-                      aria-labelledby="actionsDropdownButton"
+                      <svg
+                        className="-ml-1 mr-1.5 w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                      >
+                        <path
+                          clipRule="evenodd"
+                          fillRule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        />
+                      </svg>
+                      Actions
+                    </button>
+                    <div
+                      className={classNames(
+                        "absolute opacity-0 transition-opacity duration-300 top-10 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow",
+                        { "opacity-100": showActionsPopover }
+                      )}
+                      id="actionsDropdown"
                     >
-                      <li>
-                        <a className="block py-2 px-4 hover:bg-gray-100 :hover:bg-gray-600 :hover:text-white">
-                          Mass Edit
+                      <div className="py-1">
+                        <a className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 :hover:bg-gray-600 :text-gray-200 :hover:text-white">
+                          Delete all
                         </a>
-                      </li>
-                    </ul>
-                    <div className="py-1">
-                      <a className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 :hover:bg-gray-600 :text-gray-200 :hover:text-white">
-                        Delete all
-                      </a>
+                      </div>
                     </div>
                   </div>
                 </>
