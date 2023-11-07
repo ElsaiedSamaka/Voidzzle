7e2f75fdfadbf697ce55c0187c2ef4c56ae952b7
Saied <elsaied.samaka1997@gmail.com>
2023-11-07 11:53:25 +0200

    brands service

--- src/shared/Common/Anchor/Anchor.tsx
+++ src/shared/Common/Anchor/Anchor.tsx
@@ -1,17 +1,29 @@
+import classNames from "classnames";
+import { useThemeContext } from "core/context/ThemeContext";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 const Anchor = ({ children, href }) => {
   const pathname = usePathname();
+  const { theme } = useThemeContext();
   const active = href === pathname;
+  const { mode } = theme;
 
   return (
     <Link
       href={href}
-      className={`rounded-l-none hover:border-l-4 outline-none transition-all duration-100 ease-in-out rounded-2xl block text-sm ${
-        active
-          ? " border-l-4 border-l-black  transition-all duration-100 ease-in-out  font-bold"
-          : ""
-      }`}
+      className={classNames(
+        "rounded-l-none hover:border-l-4 outline-none transition-all duration-100 ease-in-out rounded-2xl block text-sm",
+        {
+          "border-l-4  transition-all duration-100 ease-in-out  font-bold":
+            active,
+        },
+        {
+          "border-l-light-primary": mode === "light",
+        },
+        {
+          "border-l-dark-primary": mode === "dark",
+        }
+      )}
     >
       {children}
     </Link>
