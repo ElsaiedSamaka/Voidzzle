d30e3271b6f09c38b86a8d78a68577fc1c1aa18a
Saied <elsaied.samaka1997@gmail.com>
2023-11-09 19:10:04 +0200

    code enhance

--- src/shared/Modals/Dailog/Dailog.tsx
+++ src/shared/Modals/Dailog/Dailog.tsx
@@ -1,13 +1,19 @@
+import { useThemeContext } from "core/context/ThemeContext";
 import PropTypes from "prop-types";
 
 const Dailog = ({ header, body, footer }) => {
+  const { theme } = useThemeContext();
+  const { mode } = theme;
   return (
     <>
-      {/* Main modal */}
       {/* dimmed bg */}
-      <div className="fixed z-30 inset-0 bg-gray-50 bg-opacity-40 transition-opacity md:block"></div>
+      {mode === "light" && (
+        <div className="fixed z-30 inset-0 bg-gray-50 bg-opacity-40 transition-opacity md:block" />
+      )}
       {/* blurred bg */}
-      {/* <div className="fixed inset-0 z-40 backdrop-blur-sm" /> */}
+      {mode === "dark" && (
+        <div className="fixed inset-0 z-40 backdrop-blur-sm" />
+      )}
       <div className="bg-white xs:h-full sm:h-fit max-w-4xl max-h-fit rounded-2xl z-40 fixed lg:top-[15%] lg:left-[15%] md:top-[20%] xs:top-[1%] xs:left-[5%] xs:right-[5%]  mx-auto border shadow-2xl px-5 py-5 backdrop-blur-3xl">
         <div className="flex flex-col w-full h-full max-h-fit overflow-scroll">
           {/* Modal content */}
