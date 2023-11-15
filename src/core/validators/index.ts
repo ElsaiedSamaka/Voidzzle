709281b6551c1bc6a0ff40e4894dd53f0e143582
Saied <elsaied.samaka1997@gmail.com>
2023-11-15 16:28:06 +0200

    code enhance

--- src/core/validators/index.ts
+++ src/core/validators/index.ts
@@ -5,7 +5,7 @@ const Validators = {
     return re.test(email);
   },
   password: (password: string) => {
-   const re = /(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%?&])[a-zA-Z\d@$!%*?&]{8,}/;
+   const re = /^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/;
     return re.test(password);
   },
   number: (number: string) => {
