27353af232cfeda8cc61430f2dfc5f7127ad19eb
Saied <elsaied.samaka1997@gmail.com>
2024-03-14 21:57:16 +0200

    breakpoint #9.56pm

---
+++ src/core/utils/translations/shared/general.ts
@@ -0,0 +1,16 @@
+const translations = {
+  en: {
+    "general.name": "General",
+    "general.see_all": "See all",
+    lang: "English",
+    language: "عربي",
+  },
+  ar: {
+    "general.name": "عام",
+    "general.see_all": "عرض الكل",
+    lang: "العربية",
+    language: "English",
+  },
+};
+
+export default translations;
