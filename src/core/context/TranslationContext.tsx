d8742c833b7259d527082e0f8a098e5feaad8aca
Saied <elsaied.samaka1997@gmail.com>
2024-03-16 22:22:44 +0200

    breakpoint

---
+++ src/core/context/TranslationContext.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext } from 'react';
+import translations from '../utils/translations';
+
+const TranslationContext = createContext();
+
+export const TranslationProvider = ({ children, locale }) => {
+  const t = (key) => {
+    if (!translations[locale][key]) {
+      console.warn(`Translation '${key}' for locale '${locale}' not found.`);
+    }
+    return translations[locale][key];
+  };
+
+  return (
+    <TranslationContext.Provider value={{ t }}>
+      {children}
+    </TranslationContext.Provider>
+  );
+};
+
+export const useTranslation = () => useContext(TranslationContext);
---
+++ src/views/home.views/ads.home/Ads.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Ads() {
+  return <div className="bg-gray-400 h-full p-2">ads</div>;
+}
+
+export default Ads;
