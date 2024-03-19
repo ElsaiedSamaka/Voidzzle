748a681d311a76f7d66a7a7b43f2defe82607ae4
Saied <elsaied.samaka1997@gmail.com>
2024-03-19 23:13:07 +0200

    breakpoint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,80 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
+  // Target: 'serverless' for serverless deployment
+  target: 'server',
+
+  // React Strict Mode
   reactStrictMode: true,
+
+  // i18n with next
   i18n: {
     locales: ['en', 'ar'],
     defaultLocale: 'ar',
   },
+
+  // Other experimental features
+  // experimental: {},
+
+  // Environment variables
+  env: {
+    // Define environment variables here
+  },
+
+  // Image optimization configuration
+  images: {
+    // Image optimization configuration
+  },
+
+  // Redirects configuration
+  redirects: async () => {
+    return [
+      // Define redirects here
+    ];
+  },
+
+  // Rewrites configuration
+  rewrites: async () => {
+    return [
+      // Define custom rewrites here
+    ];
+  },
+
+  // Custom headers configuration
+  headers: async () => {
+    return [
+      {
+        source: '/path',
+        headers: [
+          {
+            key: 'Custom-Header',
+            value: 'Value',
+          },
+        ],
+      },
+    ];
+  },
+
+  // Customizing the build output directory
+  distDir: 'build',
+
+  // Enable compilation of TypeScript files
+  typescript: {
+    // Enable type checking during production build
+    ignoreBuildErrors: true,
+  },
+
+  // Custom server configuration
+  serverRuntimeConfig: {
+    // Define server runtime configuration
+  },
+  publicRuntimeConfig: {
+    // Define public runtime configuration
+  },
+
+  // Trailing slash configuration
+  trailingSlash: true,
+
+  // Other configurations
+  // See: https://nextjs.org/docs/api-reference/next.config.js/introduction
 };
+
+module.exports = nextConfig;
