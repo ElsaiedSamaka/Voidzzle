cb0694ca64ee614acbef96b701d3734ee1d408c0
Saied <elsaied.samaka1997@gmail.com>
2024-03-22 19:37:02 +0200

    breakpoint

--- src/shared/Common/Header/Navbar/UserMenu/UserMenu.tsx
+++ src/shared/Common/Header/Navbar/UserMenu/UserMenu.tsx
@@ -1,20 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// packages
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Image from 'next/image';
-// hooks
 import { useThemeContext } from 'core/context/ThemeContext';
-import { useState } from 'react';
-// Models & types
-import { User } from 'core/models';
 
-const UserMenu = ({user}) => {
+const UserMenu = (props: any) => {
   const [showUserMenuDDL, setUserMenuDDL] = useState(true);
-  const { theme: { mode } } = useThemeContext();
-  console.log(user)
+  const {
+    theme: { mode },
+  } = useThemeContext();
   function toggleUserMenu() {
     setUserMenuDDL(!showUserMenuDDL);
   }
+
   return (
     <>
       <button
@@ -38,16 +36,18 @@ const UserMenu = ({user}) => {
           { 'opacity-100 -translate-x-5': showUserMenuDDL },
           {
             'bg-dark-bgSecondary text-dark-textSecondary divide-dark-border':
-              mode == 'dark',
+              mode === 'dark',
             'bg-light-bgPrimary text-light-textSecondary divide-light-border':
-              mode == 'light',
+              mode === 'light',
           },
         )}
         id="dropdown"
       >
         <div className="py-3 px-4">
-          <span className="block text-sm font-semibold  ">Neil Sims</span>
-          <span className="block text-sm  truncate ">{user?.email}</span>
+          <span className="block text-sm font-semibold  ">
+            {props.user.firstname} {props.user.lastname}
+          </span>
+          <span className="block text-sm  truncate ">{props.user.email}</span>
         </div>
         <ul className="py-1  " aria-labelledby="dropdown">
           <li>
@@ -82,7 +82,7 @@ const UserMenu = ({user}) => {
         </ul>
         <ul className="py-1  " aria-labelledby="dropdown">
           <li>
-            <a className="block py-2 px-4 text-sm hover:cursor-pointer hover:font-semibold transition-all duration-300  :">
+            <a className="block py-2 px-4 text-sm hover:cursor-pointer hover:font-semibold transition-all duration-300">
               Sign out
             </a>
           </li>
@@ -91,4 +91,27 @@ const UserMenu = ({user}) => {
     </>
   );
 };
+
+UserMenu.propTypes = {
+  user: PropTypes.shape({
+    firstname: PropTypes.string.isRequired,
+    lastname: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    username: PropTypes.string, 
+    role: PropTypes.oneOf(['user', 'admin', 'seller']).isRequired,
+    phone: PropTypes.string,
+    isEmailVerified: PropTypes.bool.isRequired,
+    address: PropTypes.string,
+    companyName: PropTypes.string,
+    password: PropTypes.string,
+    passwordConfirmation: PropTypes.string,
+    passwordChangedAt: PropTypes.instanceOf(Date),
+    profileImage: PropTypes.string,
+    discountCode: PropTypes.string,
+    socketId: PropTypes.string,
+    _id: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+
 export default UserMenu;
