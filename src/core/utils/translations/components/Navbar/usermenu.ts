4aeaacf9fa171338653506c1575ec88b5b11f83f
Saied <elsaied.samaka1997@gmail.com>
2024-03-26 07:16:07 +0200

    breakpoint

---
+++ src/core/utils/translations/components/Navbar/usermenu.ts
@@ -0,0 +1,18 @@
+const translations = {
+  en: {
+    'usermenu.My profile': 'My profile',
+    'usermenu.Account settings': 'Account settings',
+    'usermenu.My likes': 'My likes',
+    'usermenu.Sign out': 'Sign out',
+    lang: 'English',
+  },
+  ar: {
+    'usermenu.My profile': 'الحساب الشخصي',
+    'usermenu.Account settings': 'اعدادات الحساب',
+    'usermenu.My likes': 'مفضلاتي',
+    'usermenu.Sign out': 'تسجيل الخروج',
+    lang: 'العربية',
+  },
+};
+
+export default translations;
